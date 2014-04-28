fd87f5bfa27198ed47c0b4af8959cf4ccc816e5d
Mr.CrAzY <Mr.CrAzY@MrCrAzY-PC>
2014-04-28 21:06:58 +0400

    Commit

--- WebProject/src/com/analytics/Boxorchika.java
+++ WebProject/src/com/analytics/Boxorchika.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.org.apache.xml.internal.serialize.Printer;
-
 /**
  * Servlet implementation class Boxorchika
  */
